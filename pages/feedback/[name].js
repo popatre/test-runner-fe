3f053e8a0ceec834a4455a2187df7509dd292f16
popatre <76402245+popatre@users.noreply.github.com>
2022-12-09 10:38:40 +0000

    add styling to form

--- pages/feedback/[name].js
+++ pages/feedback/[name].js
@@ -28,7 +28,6 @@ function Feedback() {
                 setIsLoading(false);
             })
             .catch((err) => {
-                console.log(err, "working???");
                 setIsError(true);
                 setIsLoading(false);
             });
@@ -37,30 +36,6 @@ function Feedback() {
             deleteEvaluationFolder();
         };
     }, []);
-
-    // const runLoadingText = () => {
-    //     const messages = [
-    //         "cloning repo...",
-    //         "cloning repo...",
-    //         "setting up dbs...",
-    //         "installing dependencies...",
-    //         "installing dependencies...",
-    //         "running tests...",
-    //         "running tests...",
-    //         "running more tests...",
-    //         "writing feedback file...",
-    //         "still writing the file...",
-    //         "Almost done...",
-    //         "still going...",
-    //         "still, still going",
-    //         "still, still, still going",
-    //     ];
-    //     let i = -1;
-    //     setInterval(() => {
-    //         i++;
-    //         setLoadingText(messages[i]);
-    //     }, 2200);
-    // };
 
     if (isLoading)
         return (
