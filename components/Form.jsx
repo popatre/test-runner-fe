29102dd6a1caede64328127113136a0ca66a48bc
popatre <76402245+popatre@users.noreply.github.com>
2022-12-09 09:48:35 +0000

    split form into component

---
+++ components/Form.jsx
@@ -0,0 +1,75 @@
+import { useState, useContext } from "react";
+import styles from "../styles/Home.module.css";
+import { RequestContext } from "../context/RequestContext";
+import { useRouter } from "next/router";
+
+function Form() {
+    const { setRequest } = useContext(RequestContext);
+    const [input, setInput] = useState({ name: "", repo: "", appType: "news" });
+    const router = useRouter();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInput((prevInput) => {
+            return { ...prevInput, [name]: value };
+        });
+    };
+
+    const isFormComplete = () => {
+        return input.name && input.repo && input.appType;
+    };
+
+    const handleNav = (e) => {
+        e.preventDefault();
+        setRequest(input);
+        router.push(`/feedback/${input.name}`);
+    };
+    return (
+        <>
+            <form className={styles.form}>
+                <label>
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={input.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Github repo link:
+                    <textarea
+                        rows="2"
+                        type="text"
+                        value={input.repo}
+                        name="repo"
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    App type:
+                    <select name="appType" onChange={handleChange}>
+                        <option disabled value>
+                            {" "}
+                            -- select an app type --{" "}
+                        </option>
+                        <option value="news">NC News</option>
+                        <option value="games">NC Games</option>
+                    </select>
+                </label>
+            </form>
+
+            {isFormComplete() ? (
+                <button
+                    disabled={!isFormComplete()}
+                    className={styles.button}
+                    onClick={handleNav}
+                >
+                    Get Feedback
+                </button>
+            ) : null}
+        </>
+    );
+}
+
+export default Form;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useRouter } from "next/router";
-import { useState, useContext } from "react";
-import { RequestContext } from "../context/RequestContext";
+
+import Form from "../components/Form";
 
 export default function Home() {
-    const { setRequest } = useContext(RequestContext);
-    const router = useRouter();
-    const [input, setInput] = useState({ name: "", repo: "", appType: "news" });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setInput((prevInput) => {
-            return { ...prevInput, [name]: value };
-        });
-    };
-
-    const isFormComplete = () => {
-        return input.name && input.repo && input.appType;
-    };
-
-    const handleNav = (e) => {
-        e.preventDefault();
-        setRequest(input);
-        router.push(`/feedback/${input.name}`);
-    };
-
     return (
         <div className={styles.container}>
             <Head>
@@ -33,50 +11,9 @@ export default function Home() {
                 <meta name="description" content="Backend test runner" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
             <main className={styles.main}>
                 <h1 className={styles.title}>BE Project Test Runner</h1>
-                <form className={styles.form}>
-                    <label>
-                        Name:
-                        <input
-                            type="text"
-                            name="name"
-                            value={input.name}
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        Github repo link:
-                        <input
-                            type="text"
-                            value={input.repo}
-                            name="repo"
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        App type:
-                        {/* <input type="text" name="appType" /> */}
-                        <select name="appType" onChange={handleChange}>
-                            <option disabled value>
-                                {" "}
-                                -- select an app type --{" "}
-                            </option>
-                            <option value="news">NC News</option>
-                            <option value="games">NC Games</option>
-                        </select>
-                    </label>
-                </form>
-                {isFormComplete() ? (
-                    <button
-                        disabled={!isFormComplete()}
-                        className={styles.button}
-                        onClick={handleNav}
-                    >
-                        Get Feedback
-                    </button>
-                ) : null}
+                <Form />
             </main>
         </div>
     );
